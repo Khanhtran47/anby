69733a04ae36f6eded3ef8e69ba5fcc7fc766a90
Duc Khanh <tranduckhanh23@gmail.com>
2025-05-11 07:14:32 +0200

    feat: add eslint and prettier configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import { config as defaultConfig } from '@khanhtran47/config/eslint';
+import nextPlugin from '@next/eslint-plugin-next';
+import { globalIgnores } from 'eslint/config';
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...defaultConfig,
+	globalIgnores(['.next/**']),
+	{
+		files: ['**/*.ts', '**/*.tsx'],
+		plugins: {
+			'@next/next': nextPlugin,
+		},
+		rules: {
+			...nextPlugin.configs.recommended.rules,
+		},
+	},
 ];
 
 export default eslintConfig;
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+import { config as defaultConfig } from '@khanhtran47/config/prettier';
+
+/** @type {import("prettier").Options} */
+export const config = {
+	...defaultConfig,
+	// overrides here
+};
+
+export default config;

660a01e2c92e63f8ecfae69bb77ef10f28c1a41b
Duc Khanh <tranduckhanh23@gmail.com>
2025-05-12 17:22:30 +0200

    feat: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	images: {
+		dangerouslyAllowSVG: true,
+		unoptimized: true,
+	},
 	experimental: {
+		ppr: 'incremental',
 		reactCompiler: true,
 	},
 };

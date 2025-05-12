f4e2b565294afb971e9fdd85416db14ab6aa59d4
Duc Khanh <tranduckhanh23@gmail.com>
2025-05-12 14:51:50 +0200

    feat: add ThemeProvider component for improved theme management in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import '@/styles/globals.css';
 import { Toaster } from '@/components/ui/sonner';
 import { GlobalImageConfigsProvider } from '@/context/global-image-configs.context';
+import { ThemeProvider } from '@/context/theme-provider';
 
 export const metadata: Metadata = {
 	title: 'Project Anby',
@@ -16,14 +17,21 @@ export default function RootLayout({
 	return (
 		<html suppressHydrationWarning dir="ltr" lang="en">
 			<body className="antialiased">
-				<GlobalImageConfigsProvider
-					corsProxyEndpoint={process.env.NEXT_PUBLIC_CORS_PROXY}
-					optimizeImg={process.env.NEXT_PUBLIC_OPTIMIZE_IMAGES === 'ON'}
-					optimizerEndpoint={process.env.NEXT_PUBLIC_OPTIMIZE_IMAGES_ENDPOINT}
-					targetFormats={['webp', 'jpg', 'png']}
+				<ThemeProvider
+					disableTransitionOnChange
+					enableSystem
+					attribute="class"
+					defaultTheme="system"
 				>
-					{children}
-				</GlobalImageConfigsProvider>
+					<GlobalImageConfigsProvider
+						corsProxyEndpoint={process.env.NEXT_PUBLIC_CORS_PROXY}
+						optimizeImg={process.env.NEXT_PUBLIC_OPTIMIZE_IMAGES === 'ON'}
+						optimizerEndpoint={process.env.NEXT_PUBLIC_OPTIMIZE_IMAGES_ENDPOINT}
+						targetFormats={['webp', 'jpg', 'png']}
+					>
+						{children}
+					</GlobalImageConfigsProvider>
+				</ThemeProvider>
 				<Toaster />
 			</body>
 		</html>

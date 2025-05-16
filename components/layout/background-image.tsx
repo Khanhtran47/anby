708cda5c4d5265f18bd33862ce57e39e311531df
Duc Khanh <tranduckhanh23@gmail.com>
2025-05-16 15:58:47 +0200

    feat(background): add light and dark background images

--- components/layout/background-image.tsx
+++ components/layout/background-image.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function BackgroundImage() {
 	return (
-		<div className="absolute top-0 left-0 z-[-1] size-full bg-[url(/assets/images/bg.png)] bg-cover bg-top bg-no-repeat" />
+		<div className="absolute top-0 left-0 z-[-1] size-full bg-[url(/assets/images/bg-light.png)] bg-cover bg-top bg-no-repeat dark:bg-[url(/assets/images/bg-dark.png)]" />
 	);
 }
 

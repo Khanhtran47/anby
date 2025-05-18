cac52eaa770bc7dd3320478b87e97238252d2065
Duc Khanh <tranduckhanh23@gmail.com>
2025-05-18 15:32:48 +0200

    feat(tailwind): add responsive screen sizes to theme configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,19 @@ import tailwindBgPatterns from 'tailwindcss-bg-patterns';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+	theme: {
+		screens: {
+			'2xs': '375px',
+			xs: '425px',
+			sm: '650px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1400px',
+			'3xl': '1600px',
+			'4xl': '1920px',
+			'5xl': '2560px',
+		},
+	},
 	plugins: [tailwindBgPatterns],
 };

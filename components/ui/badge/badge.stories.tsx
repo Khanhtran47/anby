40d59a52a34c75ca255a064d756440bad05cd87e
Duc Khanh <tranduckhanh23@gmail.com>
2025-05-20 19:04:37 +0200

    feat(badge): add badge component stories and update styles for variants

---
+++ components/ui/badge/badge.stories.tsx
@@ -0,0 +1,40 @@
+import { Badge } from '@/components/ui/badge';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+/**
+ * Displays a badge or a component that looks like a badge.
+ */
+const meta = {
+	title: 'Components/Badge',
+	component: Badge,
+	tags: ['autodocs'],
+	argTypes: {
+		variant: {
+			control: {
+				type: 'select',
+			},
+			options: ['default', 'primary', 'secondary', 'destructive'],
+		},
+		children: {
+			control: 'text',
+		},
+	},
+	parameters: {
+		layout: 'centered',
+	},
+} satisfies Meta<typeof Badge>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/**
+ * The default form of the badge.
+ */
+export const Default: Story = {
+	args: {
+		variant: 'default',
+		children: 'Badge',
+	},
+};
--- components/ui/badge/badge.tsx
+++ components/ui/badge/badge.tsx
@@ -6,14 +6,13 @@ import { cn } from '@/utils/common/misc';
 import type { VariantProps } from 'tailwind-variants';
 
 const badgeVariants = tv({
-	base: 'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+	base: 'inline-flex items-center rounded-full border-2 border-background px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
 	variants: {
 		variant: {
-			default: 'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
-			secondary: 'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-			destructive:
-				'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
-			outline: 'text-foreground',
+			default: 'bg-muted text-muted-foreground',
+			primary: 'bg-primary text-primary-foreground',
+			secondary: 'bg-secondary text-secondary-foreground',
+			destructive: 'bg-destructive text-destructive-foreground',
 		},
 	},
 	defaultVariants: {

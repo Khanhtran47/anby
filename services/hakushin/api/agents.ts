4bc2e8974b918a86680c22eac3d3ef6dd40f8e1d
Duc Khanh <tranduckhanh23@gmail.com>
2025-05-25 16:01:37 +0200

    feat(agents): update list agents type and formatted agent list data

--- services/hakushin/api/agents.ts
+++ services/hakushin/api/agents.ts
@@ -5,7 +5,7 @@ import { Hakushin } from '../utils';
 import type { AgentDetails, ListAgents } from '../models/agents';
 
 export const getListAgents = async () => {
-	const result = await fetcher<ListAgents[]>({
+	const result = await fetcher<Record<string, ListAgents>>({
 		url: Hakushin.listAgents(),
 		key: 'hakushin-agents-list',
 		ttl: 1000 * 60 * 60 * 24 * 7,
@@ -15,7 +15,40 @@ export const getListAgents = async () => {
 	if (result && 'error' in result) {
 		return { error: result.error };
 	}
-	return result;
+	return Object.entries(result).map(([id, agent]) => ({
+		id: Number(id),
+		faction: agent.camp,
+		rarity: agent.rank,
+		specialty: agent.type,
+		stat: agent.element,
+		names: [
+			{
+				id: 'CHS',
+				name: agent.CHS,
+			},
+			{
+				id: 'EN',
+				name: agent.EN,
+			},
+			{
+				id: 'JA',
+				name: agent.JA,
+			},
+			{
+				id: 'KO',
+				name: agent.KO,
+			},
+		],
+		code: agent.code,
+		desc: agent.desc,
+		img: agent.icon
+			? `https://api.hakush.in/zzz/UI/IconRoleCrop${agent.icon.replace(/^IconRole/, '')}.webp`
+			: undefined,
+		skin: agent.skin,
+		spStat: agent.spelement
+			? `https://api.hakush.in/zzz/UI/${agent.spelement.replace(/^.*\/([^/]+)\.png$/, '$1')}.webp`
+			: undefined,
+	}));
 };
 
 export const getAgentDetails = async (id: string) => {

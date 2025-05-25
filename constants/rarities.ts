2442d8e329921e8f08e97647cd174aca4e09c5f4
Duc Khanh <tranduckhanh23@gmail.com>
2025-05-26 00:25:08 +0200

    feat(rarities): update rarity entries with additional icons and colors

--- constants/rarities.ts
+++ constants/rarities.ts
@@ -3,10 +3,14 @@ export const RARITIES = [
 		id: 4,
 		rarity: 'S - rank',
 		icon: 'https://anby.trandk.live/assets/images/s-rank.png',
+		icon2: 'https://anby.trandk.live/assets/images/s-rank-01.webp',
+		color: '#ffb600',
 	},
 	{
 		id: 3,
-		rarity: 'S - rank',
+		rarity: 'A - rank',
 		icon: 'https://anby.trandk.live/assets/images/a-rank.png',
+		icon2: 'https://anby.trandk.live/assets/images/a-rank-01.webp',
+		color: '#e901fe',
 	},
 ] as const;

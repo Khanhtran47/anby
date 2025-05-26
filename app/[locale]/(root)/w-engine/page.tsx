47acdadb24961137ba7348ebff99d66af251e6b5
Duc Khanh <tranduckhanh23@gmail.com>
2025-05-26 03:39:19 +0200

    feat(w-engine): implement ListWEnginePage with metadata generation and localization support

--- app/[locale]/(root)/w-engine/page.tsx
+++ app/[locale]/(root)/w-engine/page.tsx
@@ -1,7 +1,70 @@
 import React from 'react';
+import { getTranslations } from 'next-intl/server';
 
-function Page() {
-	return <div>W-Engine</div>;
+import { getListWEngine } from '@/services/hakushin/api/w-engine';
+import PageHeader from '@/components/features/page-header';
+import WEngineCard from '@/components/ui/card/w-engine-card';
+import { Image } from '@/components/ui/image';
+
+import type { Locale } from 'next-intl';
+import type { ReactNode } from 'react';
+
+type Props = {
+	children: ReactNode;
+	params: Promise<{ locale: Locale }>;
+};
+
+export async function generateMetadata(props: Omit<Props, 'children'>) {
+	const { locale } = await props.params;
+
+	const t = await getTranslations({ locale, namespace: 'WEnginePage' });
+	const tb = await getTranslations({ locale, namespace: 'Brand' });
+
+	return {
+		title: `${t('title')} | ${tb('name')}`,
+		description: t('description'),
+	};
 }
 
-export default Page;
+async function ListWEnginePage() {
+	const wEngines = await getListWEngine();
+	const t = await getTranslations('WEnginePage');
+	return (
+		<>
+			<PageHeader
+				title={t('title')}
+				rightContent={
+					<Image
+						optimizeImg
+						height={27}
+						radius="none"
+						src="https://anby.trandk.live/assets/images/zzz-logo-horizontal.png"
+						width={100}
+						classNames={{
+							wrapper: 'w-[100px] h-[27px]',
+							img: 'size-full',
+						}}
+					/>
+				}
+			/>
+			{!('error' in wEngines) ? (
+				<section className="max-w-screen-4xl mx-auto flex min-h-[850px] w-full flex-col items-center py-2 pr-4 pl-2">
+					<div className="relative my-5 grid min-h-[300px] w-full auto-cols-fr grid-flow-dense grid-cols-[repeat(auto-fill,minmax(125px,1fr))] items-stretch justify-items-center gap-3 lg:px-10">
+						{wEngines?.map((wEngine) => (
+							<WEngineCard
+								key={wEngine.id}
+								id={wEngine.id}
+								img={wEngine.icon}
+								name={wEngine.code}
+								rarity={wEngine.rarity}
+								specialty={wEngine.specialty}
+							/>
+						))}
+					</div>
+				</section>
+			) : null}
+		</>
+	);
+}
+
+export default ListWEnginePage;

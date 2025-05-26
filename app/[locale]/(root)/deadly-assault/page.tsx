a73ce0d389409de76f88fb12e779fbdfdc3e5220
Duc Khanh <tranduckhanh23@gmail.com>
2025-05-26 03:59:34 +0200

    feat(localization): add metadata generation and translations for multiple pages

--- app/[locale]/(root)/deadly-assault/page.tsx
+++ app/[locale]/(root)/deadly-assault/page.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import { getTranslations } from 'next-intl/server';
 
-function Page() {
-	return <div>Deadly Assault</div>;
+import PageHeader from '@/components/features/page-header';
+import { Image } from '@/components/ui/image';
+
+import type { Locale } from 'next-intl';
+import type { ReactNode } from 'react';
+
+type Props = {
+	children: ReactNode;
+	params: Promise<{ locale: Locale }>;
+};
+
+export async function generateMetadata(props: Omit<Props, 'children'>) {
+	const { locale } = await props.params;
+
+	const t = await getTranslations({ locale, namespace: 'DeadlyAssaultPage' });
+	const tb = await getTranslations({ locale, namespace: 'Brand' });
+
+	return {
+		title: `${t('title')} | ${tb('name')}`,
+		description: t('description'),
+	};
 }
 
-export default Page;
+async function DeadlyAssaultPage() {
+	const t = await getTranslations('DeadlyAssaultPage');
+	return (
+		<>
+			<PageHeader
+				title={t('title')}
+				rightContent={
+					<Image
+						optimizeImg
+						height={27}
+						radius="none"
+						src="https://anby.trandk.live/assets/images/zzz-logo-horizontal.png"
+						width={100}
+						classNames={{
+							wrapper: 'w-[100px] h-[27px]',
+							img: 'size-full',
+						}}
+					/>
+				}
+			/>
+		</>
+	);
+}
+
+export default DeadlyAssaultPage;
--- app/[locale]/(root)/drive-disc/page.tsx
+++ app/[locale]/(root)/drive-disc/page.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import { getTranslations } from 'next-intl/server';
 
-function Page() {
-	return <div>Drive Disc</div>;
+import PageHeader from '@/components/features/page-header';
+import { Image } from '@/components/ui/image';
+
+import type { Locale } from 'next-intl';
+import type { ReactNode } from 'react';
+
+type Props = {
+	children: ReactNode;
+	params: Promise<{ locale: Locale }>;
+};
+
+export async function generateMetadata(props: Omit<Props, 'children'>) {
+	const { locale } = await props.params;
+
+	const t = await getTranslations({ locale, namespace: 'DriveDiscPage' });
+	const tb = await getTranslations({ locale, namespace: 'Brand' });
+
+	return {
+		title: `${t('title')} | ${tb('name')}`,
+		description: t('description'),
+	};
 }
 
-export default Page;
+async function DriveDiscPage() {
+	const t = await getTranslations('DriveDiscPage');
+	return (
+		<>
+			<PageHeader
+				title={t('title')}
+				rightContent={
+					<Image
+						optimizeImg
+						height={27}
+						radius="none"
+						src="https://anby.trandk.live/assets/images/zzz-logo-horizontal.png"
+						width={100}
+						classNames={{
+							wrapper: 'w-[100px] h-[27px]',
+							img: 'size-full',
+						}}
+					/>
+				}
+			/>
+		</>
+	);
+}
+
+export default DriveDiscPage;

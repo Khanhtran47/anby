0c6f1115377dd51f9b7bcb0a8fe53012825adb60
Duc Khanh <tranduckhanh23@gmail.com>
2025-05-26 03:16:04 +0200

    chore(rarities): update rarity icons and add B-rank with new images

--- constants/rarities.ts
+++ constants/rarities.ts
@@ -3,14 +3,21 @@ export const RARITIES = [
 		id: 4,
 		rarity: 'S - rank',
 		icon: 'https://anby.trandk.live/assets/images/s-rank.png',
-		icon2: 'https://anby.trandk.live/assets/images/s-rank-01.webp',
+		icon2: 'https://anby.trandk.live/assets/images/s-rank-01.png',
 		color: '#ffb600',
 	},
 	{
 		id: 3,
 		rarity: 'A - rank',
 		icon: 'https://anby.trandk.live/assets/images/a-rank.png',
-		icon2: 'https://anby.trandk.live/assets/images/a-rank-01.webp',
+		icon2: 'https://anby.trandk.live/assets/images/a-rank-01.png',
 		color: '#e901fe',
+	},
+	{
+		id: 2,
+		rarity: 'B - rank',
+		icon: '',
+		icon2: 'https://anby.trandk.live/assets/images/b-rank-01.png',
+		color: '#00a8ff',
 	},
 ] as const;

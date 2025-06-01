c5f57f915fe0d410b336927dfbfbe733ee5662ca
Duc Khanh <tranduckhanh23@gmail.com>
2025-06-02 00:28:14 +0200

    refactor(Link): simplify onClick handling and add isExternal prop support

--- i18n/navigation.tsx
+++ i18n/navigation.tsx
@@ -1,34 +1,53 @@
 'use client';
 
-import { startTransition } from 'react';
+import { startTransition, useCallback } from 'react';
 import { createNavigation } from 'next-intl/navigation';
 
 import { useProgressBar } from '@/context/progress-bar';
 
 import { routing } from './routing';
 
-import type { ComponentProps } from 'react';
+import type { ComponentProps, MouseEvent } from 'react';
 
 const { Link: NextLink, redirect, usePathname, useRouter, getPathname } = createNavigation(routing);
 
-function Link({ href, children, onClick, ...rest }: ComponentProps<typeof NextLink>) {
+function Link({
+	href,
+	children,
+	onClick: onClickProp,
+	isExternal,
+	...rest
+}: ComponentProps<typeof NextLink> & {
+	isExternal?: boolean;
+}) {
 	const progress = useProgressBar();
 	const router = useRouter();
+	const onClick = useCallback(
+		(e: MouseEvent<HTMLAnchorElement>) => {
+			if (onClickProp) onClickProp(e);
+			e.preventDefault();
+			progress.start();
+
+			startTransition(() => {
+				router.push(href.toString());
+				progress.done();
+			});
+		},
+		[href, onClickProp, progress, router],
+	);
 
 	return (
 		<NextLink
 			href={href}
-			onClick={(e) => {
-				e.preventDefault();
-				progress.start();
-
-				startTransition(() => {
-					router.push(href.toString());
-					progress.done();
-				});
-
-				onClick?.(e);
-			}}
+			{...(isExternal
+				? {
+						target: '_blank',
+						rel: 'noopener noreferrer',
+						onClick: onClickProp,
+					}
+				: {
+						onClick,
+					})}
 			{...rest}
 		>
 			{children}

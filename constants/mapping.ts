9ae23a260f3b04fb2f0cb8aeea4ebe7566a547ad
Duc Khanh <tranduckhanh23@gmail.com>
2025-06-09 22:05:14 +0200

    feat(constants): add AGENTS_MAPPING constant

---
+++ constants/mapping.ts
@@ -0,0 +1,188 @@
+const AGENTS_MAPPING: {
+	id: number;
+	code: string;
+	hoyoId: string;
+}[] = [
+	{
+		id: 1011,
+		code: 'Anby',
+		hoyoId: '2',
+	},
+	{
+		id: 1021,
+		code: 'Nekomata',
+		hoyoId: '21',
+	},
+	{
+		id: 1031,
+		code: 'Nicole',
+		hoyoId: '20',
+	},
+	{
+		id: 1041,
+		code: 'Soldier 11',
+		hoyoId: '22',
+	},
+	{
+		id: 1061,
+		code: 'Corin',
+		hoyoId: '23',
+	},
+	{
+		id: 1071,
+		code: 'Caesar',
+		hoyoId: '381',
+	},
+	{
+		id: 1081,
+		code: 'Billy',
+		hoyoId: '19',
+	},
+	{
+		id: 1091,
+		code: 'Miyabi',
+		hoyoId: '537',
+	},
+	{
+		id: 1101,
+		code: 'Koleda',
+		hoyoId: '26',
+	},
+	{
+		id: 1111,
+		code: 'Anton',
+		hoyoId: '24',
+	},
+	{
+		id: 1121,
+		code: 'Ben',
+		hoyoId: '25',
+	},
+	{
+		id: 1131,
+		code: 'Soukaku',
+		hoyoId: '89',
+	},
+	{
+		id: 1141,
+		code: 'Lycaon',
+		hoyoId: '28',
+	},
+	{
+		id: 1151,
+		code: 'Lucy',
+		hoyoId: '125',
+	},
+	{
+		id: 1161,
+		code: 'Lighter',
+		hoyoId: '530',
+	},
+	{
+		id: 1171,
+		code: 'Burnice',
+		hoyoId: '382',
+	},
+	{
+		id: 1181,
+		code: 'Grace',
+		hoyoId: '27',
+	},
+	{
+		id: 1191,
+		code: 'Ellen',
+		hoyoId: '29',
+	},
+	{
+		id: 1201,
+		code: 'Harumasa',
+		hoyoId: '538',
+	},
+	{
+		id: 1211,
+		code: 'Rina',
+		hoyoId: '30',
+	},
+	{
+		id: 1221,
+		code: 'Yanagi',
+		hoyoId: '529',
+	},
+	{
+		id: 1241,
+		code: 'Zhu Yuan',
+		hoyoId: '31',
+	},
+	{
+		id: 1251,
+		code: 'QingYi',
+		hoyoId: '350',
+	},
+	{
+		id: 1261,
+		code: 'Jane',
+		hoyoId: '368',
+	},
+	{
+		id: 1271,
+		code: 'Seth',
+		hoyoId: '369',
+	},
+	{
+		id: 1281,
+		code: 'Piper',
+		hoyoId: '126',
+	},
+	{
+		id: 1291,
+		code: 'Hugo',
+		hoyoId: '707',
+	},
+	{
+		id: 1311,
+		code: 'Astra',
+		hoyoId: '588',
+	},
+	{
+		id: 1321,
+		code: 'Evelyn',
+		hoyoId: '589',
+	},
+	{
+		id: 1331,
+		code: 'Vivian',
+		hoyoId: '626',
+	},
+	{
+		id: 1351,
+		code: 'Pulchra',
+		hoyoId: '619',
+	},
+	{
+		id: 1361,
+		code: 'Trigger',
+		hoyoId: '620',
+	},
+	{
+		id: 1371,
+		code: 'YiXuan',
+		hoyoId: '752',
+	},
+	{
+		id: 1381,
+		code: 'Soldier 0 - Anby',
+		hoyoId: '618',
+	},
+	{
+		id: 1391,
+		code: 'Ju Fufu',
+		hoyoId: '750',
+	},
+	{
+		id: 1421,
+		code: 'Yinhu',
+		hoyoId: '751',
+	},
+];
+
+export { AGENTS_MAPPING };

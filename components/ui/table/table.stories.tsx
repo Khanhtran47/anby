69b02db6d1179ca75ed452f534a6105120edcdff
Duc Khanh <tranduckhanh23@gmail.com>
2025-06-24 01:38:21 +0200

    fix(table): update title in meta to reflect correct component hierarchy

--- components/ui/table/table.stories.tsx
+++ components/ui/table/table.stories.tsx
@@ -41,7 +41,7 @@ const invoices = [
  * Powerful table and datagrids built using TanStack Table.
  */
 const meta = {
-	title: 'ui/Table',
+	title: 'Components/Table',
 	component: Table,
 	tags: ['autodocs'],
 	argTypes: {},

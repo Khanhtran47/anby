deb4140d1aab9c6b3ede09b5205de59f8934553c
Duc Khanh <tranduckhanh23@gmail.com>
2025-06-26 14:44:51 +0200

    feat(i18n): add i18n support for gallery component

--- components/ui/gallery/gallery.tsx
+++ components/ui/gallery/gallery.tsx
@@ -7,18 +7,29 @@ import type { GalleryProps } from 'react-photoswipe-gallery';
 
 import '@/styles/pswp.css';
 
+import { useTranslations } from 'next-intl';
+
 import { isNext } from '@/utils/common/misc';
 
 function Gallery(
 	props: Omit<GalleryProps, 'options'> & {
 		options?: Omit<
 			GalleryProps['options'],
-			'arrowPrevSVG' | 'arrowNextSVG' | 'closeSVG' | 'zoomSVG'
+			| 'arrowPrevSVG'
+			| 'arrowNextSVG'
+			| 'closeSVG'
+			| 'zoomSVG'
+			| 'closeTitle'
+			| 'zoomTitle'
+			| 'arrowNextTitle'
+			| 'arrowPrevTitle'
+			| 'errorMsg'
 		>;
 	},
 ) {
 	const href = isNext ? '/assets/icons/sprite.svg' : './assets/icons/sprite.svg';
 	const { children, options, onOpen: onOpenProp, ...rest } = props;
+	const t = useTranslations('Pswp');
 
 	return (
 		<PSGallery
@@ -27,6 +38,11 @@ function Gallery(
 				arrowNextSVG: `<svg class="w-5 h-5 inline self-center text-inherit"><use href="${href}#arrow-right-bold"></use></svg>`,
 				arrowPrevSVG: `<svg class="w-5 h-5 inline self-center text-inherit"><use href="${href}#arrow-left-bold"></use></svg>`,
 				closeSVG: `<svg class="w-5 h-5 inline self-center text-inherit"><use href="${href}#close-bold"></use></svg>`,
+				closeTitle: t('close'),
+				zoomTitle: t('zoom'),
+				arrowNextTitle: t('next'),
+				arrowPrevTitle: t('previous'),
+				errorMsg: t('errorMsg'),
 			}}
 			onOpen={(pswp) => {
 				if (pswp.element) {

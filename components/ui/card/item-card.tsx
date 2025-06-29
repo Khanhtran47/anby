76fc00f8f6295203dfe77413f508352497830216
Duc Khanh <tranduckhanh23@gmail.com>
2025-06-29 02:27:07 +0200

    feat(item-card): enhance ItemCard component with customizable classNames and responsive layout

--- components/ui/card/item-card.tsx
+++ components/ui/card/item-card.tsx
@@ -23,36 +23,86 @@ interface ItemCardProps {
 	onClick?: () => void;
 	href?: string | UrlObject;
 	className?: string;
+	classNames?: {
+		card?: string;
+		content?: string;
+		imageWrapper?: string;
+		image?: string;
+		textWrapper?: string;
+		text?: string;
+	};
+	direction?: 'row' | 'col';
+	title?: string;
+	isExternalLink?: boolean;
 }
 
 function ItemCard(props: ItemCardProps) {
-	const { name, img, rarity, as, onClick, href, className } = props;
+	const {
+		name,
+		img,
+		rarity,
+		as,
+		onClick,
+		href,
+		className,
+		classNames,
+		direction = 'row',
+		title,
+		isExternalLink = false,
+	} = props;
 	const itemRarity = rarity ? RARITIES.find((r) => r.id === rarity) : undefined;
 	const Comp = as || 'div';
 	const Name = (
-		<div className="relative flex w-1/2 grow-1 items-center justify-between gap-3 p-2">
+		<div
+			className={cn(
+				'relative flex grow-1 items-center justify-between gap-3 p-2',
+				direction === 'row' ? 'w-1/2' : 'w-full',
+				classNames?.textWrapper,
+			)}
+		>
 			<div
 				className={cn(
 					'pattern-rhombus pattern-bg-pattern pattern-opacity-50 pattern-size-2 pattern-bg-background absolute top-0 left-0 z-[-1] size-full',
 				)}
 			/>
-			<span className="text-shadow-outline not-prose line-clamp-4 w-full text-center text-lg font-bold tracking-tight text-pretty select-none">
+			<span
+				className={cn(
+					'text-shadow-outline not-prose w-full text-center font-bold tracking-tight text-pretty select-none',
+					direction === 'row' ? 'line-clamp-4 text-lg' : 'line-clamp-1 text-base',
+					classNames?.text,
+				)}
+			>
 				{name}
 			</span>
 		</div>
 	);
 
 	const Content = (
-		<CardContent className="flex size-full justify-center p-0">
-			<div className="relative aspect-square h-full shrink-0 grow-0 overflow-hidden rounded-sm bg-[url('/assets/images/bg-icon.webp')] bg-cover bg-center">
+		<CardContent
+			className={cn(
+				'flex size-full justify-center p-0',
+				direction === 'row' ? 'flex-row' : 'flex-col',
+				classNames?.content,
+			)}
+		>
+			<div
+				className={cn(
+					"relative aspect-square shrink-0 grow-0 overflow-hidden rounded-sm bg-[url('/assets/images/bg-icon.webp')] bg-cover bg-center",
+					direction === 'row' ? 'h-ful' : 'h-fit',
+				)}
+			>
 				<Image
 					alt={`${name} Image`}
 					loading="lazy"
 					radius="xl"
 					src={img || '/assets/images/no-image.webp'}
 					classNames={{
-						wrapper: 'absolute left-1/2 top-1/2 -translate-1/2 w-24 aspect-square',
-						img: 'object-cover size-full',
+						wrapper: cn(
+							'absolute top-1/2 left-1/2 aspect-square -translate-1/2',
+							direction === 'row' ? 'w-24' : 'w-full',
+							classNames?.imageWrapper,
+						),
+						img: cn('size-full object-cover', classNames?.image),
 					}}
 				/>
 				{itemRarity?.color ? (
@@ -68,8 +118,10 @@ function ItemCard(props: ItemCardProps) {
 	return (
 		<Card
 			asChild
+			title={title}
 			className={cn(
-				'group bg-background aspect-[2/1] h-[150px] cursor-pointer p-4 shadow-none',
+				'group bg-background cursor-pointer p-4 shadow-none',
+				direction === 'row' ? 'aspect-[2/1] h-[150px]' : 'w-[150px]',
 				borderStyle({
 					showBorder: true,
 					borderColor: 'background',
@@ -79,6 +131,7 @@ function ItemCard(props: ItemCardProps) {
 					removeOnHover: false,
 				}),
 				className,
+				classNames?.card,
 			)}
 			onClick={onClick}
 		>
@@ -87,6 +140,7 @@ function ItemCard(props: ItemCardProps) {
 					aria-label={`View details of ${name}`}
 					className="focus-visible:ring-ring ring-offset-background focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none"
 					href={href}
+					isExternal={isExternalLink}
 				>
 					{Content}
 				</Link>

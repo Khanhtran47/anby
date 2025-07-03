56116b5adaddc3424c0a335e971670e60c64a407
Duc Khanh <tranduckhanh23@gmail.com>
2025-07-03 18:20:00 +0200

    refactor(hoyolab): update API URL handling

--- services/hoyolab/utils.ts
+++ services/hoyolab/utils.ts
@@ -1,7 +1,14 @@
-export class Hoyolab {
-	static readonly API_BASE_URL = process.env.HOYOLAB_API_URL;
+import { urlWithParams } from '@/utils/common/string';
 
-	static readonly WIKI_API_BASE_URL = process.env.HOYOLAB_WIKI_API_URL;
+type GameRecordParams = {
+	server?: string;
+	role_id?: string;
+	schedule_type?: string;
+};
+export class Hoyolab {
+	static readonly HOYOVERSE_API_URL = process.env.HOYOVERSE_API_URL;
+	static readonly HOYOLAB_API_URL = process.env.HOYOLAB_API_URL;
+	static readonly HOYOLAB_WIKI_API_URL = process.env.HOYOLAB_WIKI_API_URL;
 
 	static listNews = ({
 		langKey = 'en-us',
@@ -14,22 +21,39 @@ export class Hoyolab {
 				page?: number;
 		  }
 		| undefined = {}) => {
-		return `${this.API_BASE_URL}content_v2_user/app/3e9196a4b9274bd7/getContentList?iPageSize=${pageSize}&iPage=${page}&iChanId=288&sLangKey=${langKey}`;
+		const url = `${this.HOYOVERSE_API_URL}content_v2_user/app/3e9196a4b9274bd7/getContentList`;
+		return urlWithParams(url, {
+			iPageSize: pageSize,
+			iPage: page,
+			iChanId: 288,
+			sLangKey: langKey,
+		});
 	};
 
 	static listVideos = () => {
-		return `${this.WIKI_API_BASE_URL}hoyowiki/zzz/wapi/home/extend_reading_video`;
+		return `${this.HOYOLAB_WIKI_API_URL}hoyowiki/zzz/wapi/home/extend_reading_video`;
 	};
 
 	static entryList = () => {
-		return `${this.WIKI_API_BASE_URL}hoyowiki/zzz/wapi/get_entry_page_list`;
+		return `${this.HOYOLAB_WIKI_API_URL}hoyowiki/zzz/wapi/get_entry_page_list`;
 	};
 
 	static entryPage = ({ id }: { id: string | number }) => {
-		return `${this.WIKI_API_BASE_URL}hoyowiki/zzz/wapi/entry_page?entry_page_id=${id}`;
+		const url = `${this.HOYOLAB_WIKI_API_URL}hoyowiki/zzz/wapi/get_entry_page`;
+		return urlWithParams(url, { entry_page_id: id });
 	};
 
 	static menuFilter = ({ menuId }: { menuId: number }) => {
-		return `${this.WIKI_API_BASE_URL}hoyowiki/zzz/wapi/get_menu_filters?menu_id=${menuId}`;
+		const url = `${this.HOYOLAB_WIKI_API_URL}hoyowiki/zzz/wapi/get_menu_filters`;
+		return urlWithParams(url, { menu_id: menuId });
+	};
+
+	/**
+	 * Game Records
+	 */
+
+	static noteRecord = ({ server, role_id, schedule_type }: GameRecordParams) => {
+		const url = `${this.HOYOLAB_API_URL}game_record/app/zzz/note_record`;
+		return urlWithParams(url, { server, role_id, schedule_type });
 	};
 }

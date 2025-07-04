403e63ab78813d1409f34f5bc89970ecc20aab8f
Duc Khanh <tranduckhanh23@gmail.com>
2025-07-04 04:29:34 +0200

    feat(settings): add title and header to settings page

--- components/pages/settings/index.tsx
+++ components/pages/settings/index.tsx
@@ -5,6 +5,7 @@ import { useTranslations } from 'next-intl';
 
 import { Link } from '@/i18n/link';
 import LocaleSwitcherSelect from '@/components/features/locale-switcher-select';
+import PageHeader from '@/components/features/page-header';
 import ThemeToggle from '@/components/features/theme-toggle';
 import { Box } from '@/components/ui/box';
 import { Button } from '@/components/ui/button';
@@ -16,42 +17,40 @@ function SettingsPage() {
 	const [showAccountSettings, setShowAccountSettings] = useState(false);
 
 	return (
-		<div className="mt-1 mr-1 flex h-full flex-col items-center justify-center gap-3 pt-3 pr-2 pl-3">
-			<Box
-				fullWidth
-				className="2xs:justify-between 2xs:rounded-full 2xs:py-0 2xs:pr-0 2xs:pl-7 flex-row flex-wrap justify-start gap-3 overflow-visible"
-				showDecorImgs={false}
-				size="lg"
-				classNames={{
-					pattern: 'rounded-xl 2xs:rounded-full',
-				}}
-			>
-				<span className="not-prose s7 !font-black">{t('language')}</span>
-				<LocaleSwitcherSelect
+		<>
+			<PageHeader
+				title={t('title')}
+				rightContent={
+					<Image
+						optimizeImg
+						height={27}
+						radius="none"
+						src="https://anby.trandk.live/assets/images/zzz-logo-horizontal.png"
+						width={100}
+						classNames={{
+							wrapper: 'w-[100px] h-[27px]',
+							img: 'size-full',
+						}}
+					/>
+				}
+			/>
+			<div className="mt-15 mr-1 flex h-full flex-col items-center justify-center gap-3 pt-3 pr-2 pl-3">
+				<Box
+					fullWidth
+					className="2xs:justify-between 2xs:rounded-full 2xs:py-0 2xs:pr-0 2xs:pl-7 flex-row flex-wrap justify-start gap-3 overflow-visible"
+					showDecorImgs={false}
+					size="lg"
 					classNames={{
-						trigger: 'rounded-sm 2xs:rounded-full h-14 w-full 2xs:w-[140px] sm:w-[160px]',
+						pattern: 'rounded-xl 2xs:rounded-full',
 					}}
-				/>
-			</Box>
-			<Box
-				fullWidth
-				className="2xs:justify-between 2xs:rounded-full 2xs:py-0 2xs:pr-0 2xs:pl-7 flex-row flex-wrap justify-start gap-4 overflow-visible"
-				showDecorImgs={false}
-				size="lg"
-				classNames={{
-					pattern: 'rounded-xl 2xs:rounded-full',
-				}}
-			>
-				<span className="not-prose s7 !font-black">{t('theme')}</span>
-				<ThemeToggle
-					showValue
-					classNames={{
-						trigger: 'rounded-sm 2xs:rounded-full w-full  2xs:w-[140px] sm:w-[160px] h-14',
-						content: 'w-[140px] sm:w-[160px]',
-					}}
-				/>
-			</Box>
-			{process.env.NEXT_PUBLIC_APP_ENV === 'development' ? (
+				>
+					<span className="not-prose s7 !font-black">{t('language')}</span>
+					<LocaleSwitcherSelect
+						classNames={{
+							trigger: 'rounded-sm 2xs:rounded-full h-14 w-full 2xs:w-[140px] sm:w-[160px]',
+						}}
+					/>
+				</Box>
 				<Box
 					fullWidth
 					className="2xs:justify-between 2xs:rounded-full 2xs:py-0 2xs:pr-0 2xs:pl-7 flex-row flex-wrap justify-start gap-4 overflow-visible"
@@ -61,69 +60,89 @@ function SettingsPage() {
 						pattern: 'rounded-xl 2xs:rounded-full',
 					}}
 				>
-					<span className="not-prose s7 !font-black">Hoyolab Account</span>
-					<Dialog
-						contentHeight="full"
-						dialogTitle="Hoyolab Account Settings"
-						setShowDialog={setShowAccountSettings}
-						showDialog={showAccountSettings}
-						trigger={
-							<Button
-								className="size-14"
-								icon="chevron-right-bold"
-								onClick={() => setShowAccountSettings(true)}
-							/>
-						}
-					>
-						test
-					</Dialog>
-				</Box>
-			) : null}
-			<Box
-				fullWidth
-				className="2xs:justify-between 2xs:rounded-full 2xs:py-0 2xs:pr-0 2xs:pl-7 flex-row flex-wrap justify-start gap-4 overflow-visible"
-				showDecorImgs={false}
-				size="lg"
-				classNames={{
-					pattern: 'rounded-xl 2xs:rounded-full',
-				}}
-			>
-				<span className="not-prose s7 !font-black">{t('feedback')}</span>
-				<Button asChild className="size-14" icon="chevron-right-bold">
-					<Link isExternal href="https://github.com/Khanhtran47/anby/issues/new/choose" />
-				</Button>
-			</Box>
-			<Box fullWidth showBgCorner className="gap-3" showDecorImgs={false} size="lg">
-				<div className="flex w-full items-center justify-center gap-3">
-					<Image
-						optimizeImg
-						alt="Brand Logo"
-						height={56}
-						radius="full"
-						src="https://raw.githubusercontent.com/Khanhtran47/anby/refs/heads/main/public/assets/images/logo.webp"
-						width={56}
+					<span className="not-prose s7 !font-black">{t('theme')}</span>
+					<ThemeToggle
+						showValue
 						classNames={{
-							wrapper: 'size-14',
-							img: 'size-full object-cover',
+							trigger: 'rounded-sm 2xs:rounded-full w-full  2xs:w-[140px] sm:w-[160px] h-14',
+							content: 'w-[140px] sm:w-[160px]',
 						}}
 					/>
-					<span className="not-prose s10 !font-black">Anby</span>
-				</div>
-				<div className="flex w-full items-center justify-center gap-3">
-					<Button asChild wrapIcon icon="github">
-						<Link isExternal href="https://github.com/Khanhtran47/anby">
-							GitHub
-						</Link>
+				</Box>
+				{process.env.NEXT_PUBLIC_APP_ENV === 'development' ? (
+					<Box
+						fullWidth
+						className="2xs:justify-between 2xs:rounded-full 2xs:py-0 2xs:pr-0 2xs:pl-7 flex-row flex-wrap justify-start gap-4 overflow-visible"
+						showDecorImgs={false}
+						size="lg"
+						classNames={{
+							pattern: 'rounded-xl 2xs:rounded-full',
+						}}
+					>
+						<span className="not-prose s7 !font-black">Hoyolab Account</span>
+						<Dialog
+							contentHeight="full"
+							dialogTitle="Hoyolab Account Settings"
+							setShowDialog={setShowAccountSettings}
+							showDialog={showAccountSettings}
+							trigger={
+								<Button
+									className="size-14"
+									icon="chevron-right-bold"
+									onClick={() => setShowAccountSettings(true)}
+								/>
+							}
+						>
+							test
+						</Dialog>
+					</Box>
+				) : null}
+				<Box
+					fullWidth
+					className="2xs:justify-between 2xs:rounded-full 2xs:py-0 2xs:pr-0 2xs:pl-7 flex-row flex-wrap justify-start gap-4 overflow-visible"
+					showDecorImgs={false}
+					size="lg"
+					classNames={{
+						pattern: 'rounded-xl 2xs:rounded-full',
+					}}
+				>
+					<span className="not-prose s7 !font-black">{t('feedback')}</span>
+					<Button asChild className="size-14" icon="chevron-right-bold">
+						<Link isExternal href="https://github.com/Khanhtran47/anby/issues/new/choose" />
 					</Button>
-					<Button asChild wrapIcon icon="storybook">
-						<Link isExternal href="https://khanhtran47.github.io/anby/">
-							Storybook
-						</Link>
-					</Button>
-				</div>
-				<p className="!mt-0 max-w-5xl">{t('about')}</p>
-			</Box>
-		</div>
+				</Box>
+				<Box fullWidth showBgCorner className="gap-3" showDecorImgs={false} size="lg">
+					<div className="flex w-full items-center justify-center gap-3">
+						<Image
+							optimizeImg
+							alt="Brand Logo"
+							height={56}
+							radius="full"
+							src="https://raw.githubusercontent.com/Khanhtran47/anby/refs/heads/main/public/assets/images/logo.webp"
+							width={56}
+							classNames={{
+								wrapper: 'size-14',
+								img: 'size-full object-cover',
+							}}
+						/>
+						<span className="not-prose s10 !font-black">Anby</span>
+					</div>
+					<div className="flex w-full items-center justify-center gap-3">
+						<Button asChild wrapIcon icon="github">
+							<Link isExternal href="https://github.com/Khanhtran47/anby">
+								GitHub
+							</Link>
+						</Button>
+						<Button asChild wrapIcon icon="storybook">
+							<Link isExternal href="https://khanhtran47.github.io/anby/">
+								Storybook
+							</Link>
+						</Button>
+					</div>
+					<p className="!mt-0 max-w-5xl">{t('about')}</p>
+				</Box>
+			</div>
+		</>
 	);
 }
 
